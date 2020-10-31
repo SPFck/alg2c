585c168169dc22f94ea52a436b055a2af538f998
SPFck <73548496+SPFck@users.noreply.github.com>
2020-10-31 00:57:36 +0000

    Update alg2c.h

--- alg2c.h
+++ alg2c.h
@@ -2,3 +2,12 @@
 #define Escrever printf
 #define Ler scanf
 #define Enquanto while
+#define inteiro int
+#define retornar return
+#define para for
+#define SeNão else
+#define nulo NULL
+int system(const char *com);
+#define Executar system
+#define constante const
+#define Ficheiro FILE
